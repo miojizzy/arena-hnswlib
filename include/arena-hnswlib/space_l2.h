afedad1b2e2471869635b26602d3f9e71f55ae0a
miojizzy <908678961@qq.com>
2026-02-10 02:53:05 +0000

    fix

--- include/arena-hnswlib/space_l2.h
+++ include/arena-hnswlib/space_l2.h
@@ -18,12 +18,92 @@ inline static dist_t L2Squared(const dist_t* pVect1, const dist_t* pVect2, size_
     return res;
 }
 
+#if defined(USE_AVX)
+template<typename dist_t>
+dist_t L2SqrSIMDAVX(const dist_t *pVect1, const dist_t *pVect2, const size_t dim);
+#endif
+
+#if defined(USE_AVX)
+template<>
+float L2SqrSIMDAVX<float>(const float *pVect1, const float *pVect2, const size_t dim) {
+    float PORTABLE_ALIGN64 TmpRes[8];
+    const float *pEnd1 = pVect1 + (dim >> 4 << 4);
+    __m256 diff, v1, v2;    
+    __m256 sum = _mm256_set1_ps(0);
+    while (pVect1 < pEnd1) {
+        v1 = _mm256_load_ps(pVect1);
+        pVect1 += 8;
+        v2 = _mm256_load_ps(pVect2);
+        pVect2 += 8;
+        diff = _mm256_sub_ps(v1, v2);
+        sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+
+        v1 = _mm256_load_ps(pVect1);
+        pVect1 += 8;
+        v2 = _mm256_load_ps(pVect2);
+        pVect2 += 8;
+        diff = _mm256_sub_ps(v1, v2);
+        sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+    }
+    _mm256_store_ps(TmpRes, sum);
+    return TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] + TmpRes[4] + TmpRes[5] + TmpRes[6] + TmpRes[7];
+}
+
+template<>
+double L2SqrSIMDAVX<double>(const double *pVect1, const double *pVect2, const size_t dim) {
+    double PORTABLE_ALIGN64 TmpRes[4];
+    const double *pEnd1 = pVect1 + (dim >> 4 << 4);
+    __m256d diff, v1, v2;
+    __m256d sum = _mm256_set1_pd(0);
+    while (pVect1 < pEnd1) {
+        v1 = _mm256_load_pd(pVect1);
+        pVect1 += 4;
+        v2 = _mm256_load_pd(pVect2);
+        pVect2 += 4;
+        diff = _mm256_sub_pd(v1, v2);
+        sum = _mm256_add_pd(sum, _mm256_mul_pd(diff, diff));
+
+        v1 = _mm256_load_pd(pVect1);
+        pVect1 += 4;
+        v2 = _mm256_load_pd(pVect2);
+        pVect2 += 4;
+        diff = _mm256_sub_pd(v1, v2);
+        sum = _mm256_add_pd(sum, _mm256_mul_pd(diff, diff));
+    }
+    _mm256_store_pd(TmpRes, sum);
+    return TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3];
+}
+#endif
+
+#if defined(USE_AVX)
+template<typename dist_t>
+static dist_t
+L2SqrSIMD16ExtResiduals(const dist_t *pVect1, const dist_t *pVect2, const size_t dim) {
+    size_t dim16 = dim >> 4 << 4;
+    dist_t res = L2SqrSIMDAVX<dist_t>(pVect1, pVect2, dim16);
+    const dist_t *pVect1_tail = pVect1 + dim16;
+    const dist_t *pVect2_tail = pVect2 + dim16;
+
+    size_t qty_left = dim - dim16;
+    dist_t res_tail = L2Squared(pVect1_tail, pVect2_tail, qty_left);
+    return (res + res_tail);
+}
+#endif
+
 // Squared L2 (Euclidean) distance function
 // Returns sum of squared differences between two vectors
 template<typename dist_t> 
 inline static dist_t 
 L2SquaredDistance(const dist_t *pVect1, const dist_t *pVect2, const size_t dim) {
+    #if defined(USE_AVX)
+    if (dim % 16 == 0) {
+        return L2SqrSIMDAVX<dist_t>(pVect1, pVect2, dim);
+    } else {
+        return L2SqrSIMD16ExtResiduals<dist_t>(pVect1, pVect2, dim);
+    }
+    #else
     return L2Squared<dist_t>(pVect1, pVect2, dim);
+    #endif
 }
 
 
